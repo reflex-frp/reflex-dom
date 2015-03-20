1cf618c903478413d485c2a2e98bc6cccedea709
Doug Beardsley <mightybyte@gmail.com>
2015-03-20 19:01:56 -0400

    Remove commented code

--- src/Reflex/Dom/Widget/Input.hs
+++ src/Reflex/Dom/Widget/Input.hs
@@ -50,48 +50,6 @@ input' inputType initial eSetValue dAttrs = do
   dValue <- holdDyn initial $ leftmost [eSetValue, eChange]
   return $ TextInput dValue eKeypress eKeydown eKeyup dFocus e
 
-{-  
-  dSetValue <- holdDyn "" eSetValue
-  dynText =<< combineDyn (\v a -> "Input placeholder: " <> show (inputType, v, a)) dSetValue dAttrs
-  return $ TextInput (constDyn "") never never never (constDyn False) (constDyn Nothing)
-
-input' inputType eSetValue dAttrs = do
-  dEffectiveAttrs <- mapDyn (Map.insert "type" inputType) dAttrs
-  let mkSelf (initialValue, initialAttrs) = do
-        doc <- askDocument
-        Just e <- liftIO $ liftM (fmap castToHTMLInputElement) $ documentCreateElement doc "input"
-        liftIO $ htmlInputElementSetValue e initialValue
-        iforM_ initialAttrs $ \attr value -> liftIO $ elementSetAttribute e attr value
-        eChange <- wrapDomEvent e elementOninput $ liftIO $ htmlInputElementGetValue e
-        runFrame <- askRunFrame
-        eChangeFocus <- newEventWithTrigger $ \eChangeFocusTrigger -> do
-          unsubscribeOnblur <- liftIO $ elementOnblur e $ liftIO $ do
-            runFrame $ DMap.singleton eChangeFocusTrigger False
-          unsubscribeOnfocus <- liftIO $ elementOnfocus e $ liftIO $ do
-            runFrame $ DMap.singleton eChangeFocusTrigger True
-          return $ liftIO $ unsubscribeOnblur >> unsubscribeOnfocus
-        eKeypress <- wrapDomEvent e elementOnkeypress $ liftIO . uiEventGetKeyCode =<< event
-        eKeydown <- wrapDomEvent e elementOnkeydown $ liftIO . uiEventGetKeyCode =<< event
-        eKeyup <- wrapDomEvent e elementOnkeyup $ liftIO . uiEventGetKeyCode =<< event
-        return (e, eChange, eKeypress, eKeydown, eKeyup, eChangeFocus)
-  dInitialValue <- holdDyn "" eSetValue
-  dInitial <- combineDyn (,) dInitialValue dEffectiveAttrs
-  eCreated <- performEvent . fmap mkSelf . tagDyn dInitial =<< getEInit
-  dMyElement <- holdDyn Nothing $ fmap (Just . (^. _1)) eCreated
-  performEvent_ . updated =<< combineDyn (maybe (const $ return ()) $ \e attrs -> liftIO $ setElementAttributes e attrs) dMyElement dEffectiveAttrs
-  putEChildren $ fmap ((:[]) . toNode . (^. _1)) eCreated
-  performEvent_ $ fmapMaybe (fmap $ \(e,v) -> liftIO $ htmlInputElementSetValue e v) $ attachWith (\e v -> case e of
-    Nothing -> Nothing
-    Just e' -> Just (e',v)) (current dMyElement) eSetValue
-  eChange <- liftM switch $ hold never $ fmap (^. _2) eCreated
-  dFocus <- holdDyn False . switch =<< hold never (fmap (^. _6) eCreated) --TODO: Check that 'False' is always the correct starting value - i.e.: an element will always receive an 'onfocus' event, even if it gets focus immediately
-  dValue <- holdDyn "" $ leftmost [eSetValue, eChange]
-  eKeypress <- liftM switch $ hold never $ fmap (^. _3) eCreated
-  eKeydown <- liftM switch $ hold never $ fmap (^. _4) eCreated
-  eKeyup <- liftM switch $ hold never $ fmap (^. _5) eCreated
-  return $ TextInput dValue eKeypress eKeydown eKeyup dFocus dMyElement
--}
-
 data TextInput t
   = TextInput { _textInput_value :: Dynamic t String
               , _textInput_keypress :: Event t Int
@@ -130,43 +88,41 @@ textArea initial eSet attrs = do
   v <- holdDyn "" ev
   return $ TextArea v e f
 
-{-
-type family Controller sm t a where
-  Controller Edit t a = (a, Event t a) -- Initial value and setter
-  Controller View t a = Dynamic t a -- Value (always)
-
-type family Output sm t a where
-  Output Edit t a = Dynamic t a -- Value (always)
-  Output View t a = Event t a -- Requested changes
-
-data CheckboxConfig sm t
-   = CheckboxConfig { _checkbox_input :: Controller sm t Bool
-                    , _checkbox_attributes :: Attributes
-                    }
-
-instance Reflex t => Default (CheckboxConfig Edit t) where
-  def = CheckboxConfig (False, never) mempty
-
-data Checkbox sm t
-  = Checkbox { _checkbox_output :: Output sm t Bool
-             }
-
-data StateMode = Edit | View
-
---TODO: There must be a more generic way to get this witness and allow us to case on the type-level StateMode
-data StateModeWitness (sm :: StateMode) where
-  EditWitness :: StateModeWitness Edit
-  ViewWitness :: StateModeWitness View
-
-class HasStateModeWitness (sm :: StateMode) where
-  stateModeWitness :: StateModeWitness sm
-
-instance HasStateModeWitness Edit where
-  stateModeWitness = EditWitness
-
-instance HasStateModeWitness View where
-  stateModeWitness = ViewWitness
--}
+-- type family Controller sm t a where
+--   Controller Edit t a = (a, Event t a) -- Initial value and setter
+--   Controller View t a = Dynamic t a -- Value (always)
+-- 
+-- type family Output sm t a where
+--   Output Edit t a = Dynamic t a -- Value (always)
+--   Output View t a = Event t a -- Requested changes
+-- 
+-- data CheckboxConfig sm t
+--    = CheckboxConfig { _checkbox_input :: Controller sm t Bool
+--                     , _checkbox_attributes :: Attributes
+--                     }
+-- 
+-- instance Reflex t => Default (CheckboxConfig Edit t) where
+--   def = CheckboxConfig (False, never) mempty
+-- 
+-- data Checkbox sm t
+--   = Checkbox { _checkbox_output :: Output sm t Bool
+--              }
+-- 
+-- data StateMode = Edit | View
+-- 
+-- --TODO: There must be a more generic way to get this witness and allow us to case on the type-level StateMode
+-- data StateModeWitness (sm :: StateMode) where
+--   EditWitness :: StateModeWitness Edit
+--   ViewWitness :: StateModeWitness View
+-- 
+-- class HasStateModeWitness (sm :: StateMode) where
+--   stateModeWitness :: StateModeWitness sm
+-- 
+-- instance HasStateModeWitness Edit where
+--   stateModeWitness = EditWitness
+-- 
+-- instance HasStateModeWitness View where
+--   stateModeWitness = ViewWitness
 
 data CheckboxConfig t
    = CheckboxConfig { _checkboxConfig_setValue :: Event t Bool
