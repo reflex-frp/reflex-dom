204e4ec954fc3add3154e2cf3f2660c5ffd3fcac
Ryan Trinkle <ryan.trinkle@gmail.com>
2015-09-18 16:41:52 -0400

    Revert "Make HasAttributes more polymorphic"
    This reverts commit c24dbf065cfd18de88fd0131649916086ea430bd.

--- src/Reflex/Dom/Widget/Basic.hs
+++ src/Reflex/Dom/Widget/Basic.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ScopedTypeVariables, LambdaCase, ConstraintKinds, TypeFamilies, FlexibleContexts, MultiParamTypeClasses, FlexibleInstances, RecursiveDo, GADTs, DataKinds, RankNTypes, TemplateHaskell, FunctionalDependencies #-}
+{-# LANGUAGE ScopedTypeVariables, LambdaCase, ConstraintKinds, TypeFamilies, FlexibleContexts, MultiParamTypeClasses, FlexibleInstances, RecursiveDo, GADTs, DataKinds, RankNTypes, TemplateHaskell #-}
 
 module Reflex.Dom.Widget.Basic where
 
@@ -651,11 +651,11 @@ emptyElWith' elementTag cfg = do
 elDynAttrNS' :: forall t m a. MonadWidget t m => Maybe String -> String -> Dynamic t (Map String String) -> m a -> m (El t, a)
 elDynAttrNS' mns elementTag attrs = elWith' elementTag $
   def & namespace .~ mns
-      & attributes .~ attrs
+      & elConfig_attributes .~ attrs
 
 {-# INLINABLE elDynAttr' #-}
 elDynAttr' :: forall t m a. MonadWidget t m => String -> Dynamic t (Map String String) -> m a -> m (El t, a)
-elDynAttr' elementTag attrs = elWith' elementTag $ def & attributes .~ attrs
+elDynAttr' elementTag attrs = elWith' elementTag $ def & elConfig_attributes .~ attrs
 
 {-# INLINABLE elAttr #-}
 elAttr :: forall t m a. MonadWidget t m => String -> Map String String -> m a -> m a
@@ -671,7 +671,7 @@ elAttr' elementTag attrs = elWith' elementTag $ def & attributes .~ attrs
 
 {-# INLINABLE elDynAttr #-}
 elDynAttr :: forall t m a. MonadWidget t m => String -> Dynamic t (Map String String) -> m a -> m a
-elDynAttr elementTag attrs = elWith elementTag $ def & attributes .~ attrs
+elDynAttr elementTag attrs = elWith elementTag $ def & elConfig_attributes .~ attrs
 
 {-# INLINABLE el #-}
 el :: forall t m a. MonadWidget t m => String -> m a -> m a
@@ -713,10 +713,12 @@ data Link t
   = Link { _link_clicked :: Event t ()
          }
 
-class HasAttributes s t a b | s -> a, t -> b, s b -> t, t a -> s where
-  attributes :: Lens s t a b
+class HasAttributes a where
+  type Attrs a :: *
+  attributes :: Lens' a (Attrs a)
 
-instance HasAttributes (ElConfig attrs) (ElConfig attrs') attrs attrs' where
+instance HasAttributes (ElConfig attrs) where
+  type Attrs (ElConfig attrs) = attrs
   attributes = elConfig_attributes
 
 class HasNamespace a where
--- src/Reflex/Dom/Widget/Input.hs
+++ src/Reflex/Dom/Widget/Input.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ConstraintKinds, TypeFamilies, FlexibleContexts, DataKinds, GADTs, ScopedTypeVariables, FlexibleInstances, RecursiveDo, TemplateHaskell, MultiParamTypeClasses #-}
+{-# LANGUAGE ConstraintKinds, TypeFamilies, FlexibleContexts, DataKinds, GADTs, ScopedTypeVariables, FlexibleInstances, RecursiveDo, TemplateHaskell #-}
 module Reflex.Dom.Widget.Input (module Reflex.Dom.Widget.Input, def, (&), (.~)) where
 
 import Prelude
@@ -222,16 +222,20 @@ liftM concat $ mapM makeLenses
   , ''Checkbox
   ]
 
-instance HasAttributes (TextAreaConfig t) (TextAreaConfig t) (Dynamic t (Map String String)) (Dynamic t (Map String String)) where
+instance HasAttributes (TextAreaConfig t) where
+  type Attrs (TextAreaConfig t) = Dynamic t (Map String String)
   attributes = textAreaConfig_attributes
 
-instance HasAttributes (TextInputConfig t) (TextInputConfig t) (Dynamic t (Map String String)) (Dynamic t (Map String String)) where
+instance HasAttributes (TextInputConfig t) where
+  type Attrs (TextInputConfig t) = Dynamic t (Map String String)
   attributes = textInputConfig_attributes
 
-instance HasAttributes (DropdownConfig t k) (DropdownConfig t k) (Dynamic t (Map String String)) (Dynamic t (Map String String)) where
+instance HasAttributes (DropdownConfig t k) where
+  type Attrs (DropdownConfig t k) = Dynamic t (Map String String)
   attributes = dropdownConfig_attributes
 
-instance HasAttributes (CheckboxConfig t) (CheckboxConfig t) (Dynamic t (Map String String)) (Dynamic t (Map String String)) where
+instance HasAttributes (CheckboxConfig t) where
+  type Attrs (CheckboxConfig t) = Dynamic t (Map String String)
   attributes = checkboxConfig_attributes
 
 class HasSetValue a where
