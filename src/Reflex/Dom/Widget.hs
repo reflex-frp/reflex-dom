fb5eebfbd28f51f6a0403b71ae9f8af370532b65
Ali Abrar <aliabrar@gmail.com>
2015-10-07 16:42:33 -0400

    add resizeDetector

--- src/Reflex/Dom/Widget.hs
+++ src/Reflex/Dom/Widget.hs
@@ -1,8 +1,10 @@
 module Reflex.Dom.Widget ( module Reflex.Dom.Widget.Basic
                          , module Reflex.Dom.Widget.Input
                          , module Reflex.Dom.Widget.Lazy
+                         , module Reflex.Dom.Widget.Resize
                          ) where
 
 import Reflex.Dom.Widget.Basic
 import Reflex.Dom.Widget.Input
 import Reflex.Dom.Widget.Lazy
+import Reflex.Dom.Widget.Resize
