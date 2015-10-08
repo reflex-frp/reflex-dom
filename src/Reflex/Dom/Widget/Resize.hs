9cfb7d913671f9142f78a25de4184759f54b01e0
Ali Abrar <aliabrar@gmail.com>
2015-10-07 23:13:07 -0400

    add resizeDetectorWithStyle

--- src/Reflex/Dom/Widget/Resize.hs
+++ src/Reflex/Dom/Widget/Resize.hs
@@ -13,10 +13,16 @@ import GHCJS.DOM.Element
 -- | A widget that wraps the given widget in a div and fires an event when resized.
 --   Adapted from github.com/marcj/css-element-queries
 resizeDetector :: MonadWidget t m => m a -> m (Event t (), a)
-resizeDetector w = do
+resizeDetector = resizeDetectorWithStyle ""
+
+resizeDetectorWithStyle :: MonadWidget t m
+  => String -- ^ A css style string. Warning: It should not contain the "position" style attribute.
+  -> m a -- ^ The embedded widget
+  -> m (Event t (), a) -- ^ An 'Event' that fires on resize, and the result of the embedded widget
+resizeDetectorWithStyle styleString w = do
   let childStyle = "position: absolute; left: 0; top: 0;"
       containerAttrs = "style" =: "position: absolute; left: 0; top: 0; right: 0; bottom: 0; overflow: scroll; z-index: -1; visibility: hidden;"
-  (parent, (expand, expandChild, shrink, w')) <- elAttr' "div" ("style" =: "position: relative;") $ do
+  (parent, (expand, expandChild, shrink, w')) <- elAttr' "div" ("style" =: ("position: relative;" <> styleString)) $ do
     w' <- w
     elAttr "div" containerAttrs $ do
       (expand, (expandChild, _)) <- elAttr' "div" containerAttrs $ elAttr' "div" ("style" =: childStyle) $ return ()
