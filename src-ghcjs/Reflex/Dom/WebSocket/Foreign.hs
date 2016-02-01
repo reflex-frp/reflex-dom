e9233660f2b9ded0c0d72cbdedc5e60200794378
Attila Sztupak <attila.sztupak@gmail.com>
2016-02-01 17:27:37 +0000

    websockets allow receiving text messages not only binaries

--- src-ghcjs/Reflex/Dom/WebSocket/Foreign.hs
+++ src-ghcjs/Reflex/Dom/WebSocket/Foreign.hs
@@ -6,6 +6,8 @@ import Prelude hiding (div, span, mapM, mapM_, concat, concatMap, all, sequence)
 
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
+import Data.Text as T
+import Data.Text.Encoding (encodeUtf8)
 import GHCJS.Types
 import GHCJS.DOM.WebSocket (message, open, closeEvent)
 import qualified GHCJS.DOM.WebSocket as GD
@@ -17,6 +19,8 @@ import Control.Monad.Reader
 import GHCJS.Buffer
 import JavaScript.TypedArray.ArrayBuffer as JS
 import GHCJS.Marshal.Pure
+import qualified GHCJS.Prim as Prim
+import GHCJS.Foreign
 
 data JSWebSocket = JSWebSocket { unWebSocket :: WebSocket }
 
@@ -28,8 +32,12 @@ newWebSocket _ url onMessage onOpen onClose = do
   _ <- on ws message $ do
     e <- ask
     d <- getData e
-    ab <- liftIO $ unsafeFreeze $ pFromJSVal d
-    liftIO $ onMessage $ toByteString 0 Nothing $ createFromArrayBuffer ab
+    if isString d
+      then
+        liftIO $ onMessage $ encodeUtf8 $ T.pack $ Prim.fromJSString d
+      else do
+        ab <- liftIO $ unsafeFreeze $ pFromJSVal d
+        liftIO $ onMessage $ toByteString 0 Nothing $ createFromArrayBuffer ab
   _ <- on ws closeEvent $ liftIO onClose
   return $ JSWebSocket ws
 
