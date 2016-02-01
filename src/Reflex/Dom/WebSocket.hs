c18a0a57f825af72d55b3c1b1398ce02b8fbf952
Attila Sztupak <attila.sztupak@gmail.com>
2016-02-01 17:26:35 +0000

    websockets: queue messages while not open
    also fixes messages sent while ws connecting

--- src/Reflex/Dom/WebSocket.hs
+++ src/Reflex/Dom/WebSocket.hs
@@ -10,11 +10,15 @@ import Reflex.Dom.Class
 import Reflex.Dom.WebSocket.Foreign
 
 import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Exception (catch, SomeException)
 import Control.Lens
 import Control.Monad hiding (forM, forM_, mapM, mapM_, sequence)
 import Control.Monad.IO.Class
 import Control.Monad.Ref
 import Control.Monad.State
+import Control.Arrow (second)
+import Data.Maybe (isJust)
 import Data.ByteString (ByteString)
 import Data.Default
 import Data.Dependent.Map (DSum (..))
@@ -41,6 +45,8 @@ webSocket url config = do
   currentSocketRef <- liftIO $ newIORef Nothing
   --TODO: Disconnect if value no longer needed
   (eOpen, eOpenTriggerRef) <- newEventWithTriggerRef
+  payloadQueue <- liftIO newTQueueIO
+  isOpen       <- liftIO newEmptyTMVarIO
   let onMessage :: ByteString -> IO ()
       onMessage m = postGui $ do
         mt <- readRef eRecvTriggerRef
@@ -48,21 +54,34 @@ webSocket url config = do
       onOpen = postGui $ do
         mt <- readRef eOpenTriggerRef
         forM_ mt $ \t -> runWithActions [t :=> Identity ()]
+        liftIO $ void $ atomically $ tryPutTMVar isOpen ()
+      --TODO: Is the fork necessary, or do event handlers run in their own threads automatically?
+      onClose = void $ forkIO $ do
+        liftIO $ atomically $ tryTakeTMVar isOpen
+        liftIO $ writeIORef currentSocketRef Nothing
+        liftIO $ threadDelay 1000000
+        start
       start = do
-        ws <- liftIO $ newWebSocket wv url onMessage onOpen $ do
-          void $ forkIO $ do --TODO: Is the fork necessary, or do event handlers run in their own threads automatically?
-            liftIO $ writeIORef currentSocketRef Nothing
-            liftIO $ threadDelay 1000000
-            start
+        ws <- liftIO $ newWebSocket wv url onMessage onOpen onClose
         liftIO $ writeIORef currentSocketRef $ Just ws
         return ()
   schedulePostBuild $ liftIO start
-  performEvent_ $ ffor (_webSocketConfig_send config) $ \payloads -> forM_ payloads $ \payload -> do
+  performEvent_ $ ffor (_webSocketConfig_send config) $ \payloads -> forM_ payloads $ \payload ->
+    liftIO $ atomically $ writeTQueue payloadQueue payload
+
+  liftIO $ forkIO $ forever $ do
+    payload <- atomically $ do
+      pl     <- readTQueue payloadQueue
+      open   <- tryReadTMVar isOpen
+      if isJust open then return pl else retry
+
     mws <- liftIO $ readIORef currentSocketRef
-    case mws of
-      Nothing -> return () -- Discard --TODO: should we do something better here? probably buffer it, since we handle reconnection logic; how do we verify that the server has received things?
-      Just ws -> do
-        liftIO $ webSocketSend ws payload
+    succ <- case mws of
+      Nothing -> return False
+      Just ws -> liftIO ((webSocketSend ws payload >> return True)
+                         `catch`
+                         (\(_ :: SomeException) -> return False))
+    unless succ $ atomically $ unGetTQueue payloadQueue payload
   return $ WebSocket eRecv eOpen
 
 makeLensesWith (lensRules & simpleLenses .~ True) ''WebSocketConfig
