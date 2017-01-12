62f776d299b005fde097961be468200eda8f1cde
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2017-01-13 03:18:06 +1300

    Fix for iOS

--- reflex-dom/src/Reflex/Dom/Internal.hs
+++ reflex-dom/src/Reflex/Dom/Internal.hs
@@ -19,7 +19,7 @@ import qualified Reflex.Dom.Main as Main
 #if defined(ghcjs_HOST_OS)
 run :: a -> a
 run = id
-#elif defined(darwin_HOST_OS)
+#elif defined(MIN_VERSION_jsaddle_wkwebview)
 import Language.Javascript.JSaddle.WKWebView (run)
 #else
 import Language.Javascript.JSaddle.WebKitGTK (run)
