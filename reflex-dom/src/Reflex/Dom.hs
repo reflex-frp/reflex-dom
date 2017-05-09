aadda85dd92a08a497255f2040190059e3b7c00d
Ryan Trinkle <ryan.trinkle@gmail.com>
2017-05-08 22:44:58 -0400

    Pass along Reflex.Dom.Orphans

--- reflex-dom/src/Reflex/Dom.hs
+++ reflex-dom/src/Reflex/Dom.hs
@@ -3,4 +3,4 @@ module Reflex.Dom (module X) where
 import Reflex.Dom.Core as X hiding (mainWidget, mainWidgetInElementById, mainWidgetWithCss,
                              mainWidgetWithHead, mainWidgetWithHead', runApp')
 import Reflex.Dom.Internal as X
-
+import Reflex.Dom.Orphans ()
