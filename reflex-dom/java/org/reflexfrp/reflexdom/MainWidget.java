24d2e99ac2613f95b4639d54857a9474de244e31
hsloan <ishaqsloan@gmail.com>
2018-11-19 22:48:21 +0000

    enable Geolocation in WebView Settings

--- reflex-dom/java/org/reflexfrp/reflexdom/MainWidget.java
+++ reflex-dom/java/org/reflexfrp/reflexdom/MainWidget.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.webkit.CookieManager;
+import android.webkit.GeolocationPermissions;
 import android.webkit.JavascriptInterface;
 import android.webkit.MimeTypeMap;
 import android.webkit.PermissionRequest;
@@ -41,6 +42,7 @@ public class MainWidget {
     ws.setAllowFileAccessFromFileURLs(true);
     ws.setAllowUniversalAccessFromFileURLs(true);
     ws.setDomStorageEnabled(true);
+    ws.setGeolocationEnabled(true);
     wv.setWebContentsDebuggingEnabled(true);
     // allow video to play without user interaction
     wv.getSettings().setMediaPlaybackRequiresUserGesture(false);
@@ -108,6 +110,11 @@ public class MainWidget {
         @Override
         public Bitmap getDefaultVideoPoster() {
             return Bitmap.createBitmap(10, 10, Bitmap.Config.ARGB_8888);
+        }
+
+        @Override
+        public void onGeolocationPermissionsShowPrompt (String origin, GeolocationPermissions.Callback callback) {
+          callback.invoke(origin, true, true);
         }
     });
 
