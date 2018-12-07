08d79f989a3837c7141d99ab8213a379aed95402
Luigy Leon <luigy.leon@obsidian.systems>
2018-12-07 21:54:23 +0000

    Fix typo

--- reflex-dom-core/test/hydration.hs
+++ reflex-dom-core/test/hydration.hs
@@ -691,7 +691,7 @@ testWidget' beforeJS afterSwitchover bodyWidget = maybe (error "test timed out")
       jsaddleWarp = forkIO $ Warp.runSettings settings application
   bracket jsaddleWarp killThread $ \_ -> do
     browserPath <- T.strip . T.pack <$> readProcess "which" [ "chromium" ] ""
-    when (T.null browserPath) $ fail "No browser was not found"
+    when (T.null browserPath) $ fail "No browser was found"
     WD.runSession (chromeConfig browserPath) . WD.finallyClose $ do
       WD.openPage $ "http://localhost:" <> show port
       a <- beforeJS
