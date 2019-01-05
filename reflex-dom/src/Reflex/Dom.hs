cd0d9523116bad4dfc7a4763d2b714f3385e866d
Luigy Leon <luigy.leon@obsidian.systems>
2019-01-05 05:53:16 +0000

    Fix haddocks for reflex-dom

--- reflex-dom/src/Reflex/Dom.hs
+++ reflex-dom/src/Reflex/Dom.hs
@@ -1,3 +1,10 @@
+-- Disable haddocks on this module due to a bug on haddocks when selectively
+-- reexporting on ghc > 8.2.
+-- https://github.com/haskell/haddock/issues/979
+{-# LANGUAGE CPP #-}
+#if __GLASGOW_HASKELL__ < 840
+{-# OPTIONS_HADDOCK hide, prune, ignore-exports #-}
+#endif
 module Reflex.Dom (module X) where
 
 import Foreign.JavaScript.Orphans ()
