f1915a1d5a0f33196f7418f601b4e293195de2c2
Luigy Leon <luichi.19@gmail.com>
2019-01-29 01:24:54 -0500

    Add jobs per system

--- release.nix
+++ release.nix
@@ -2,7 +2,11 @@
 }:
 let
   inherit (rp.nixpkgs) lib;
+  systems = ["x86_64-linux" "x86_64-darwin"];
   compilers = ["ghc8_4" "ghc8_0" "ghcjs8_4" "ghcjs8_0"];
-in lib.genAttrs compilers (ghc: {
-  inherit (import ./. rp.${ghc} rp.nixpkgs) reflex-dom reflex-dom-core;
-})
+in lib.genAttrs systems (system:
+  lib.genAttrs compilers (ghc: let
+    reflex-platform = import ./reflex-platform.nix { inherit system; };
+    hsPkgs = reflex-platform.${ghc};
+  in { inherit (import ./. hsPkgs reflex-platform.nixpkgs) reflex-dom reflex-dom-core; })
+)
