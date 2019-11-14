9a77bfe7f232cf35e2523106165ac264d0d5b3a7
Ali Abrar <aliabrar@gmail.com>
2019-11-13 21:11:09 -0500

    reflex-dom-core: Allow superfluous nix arguments

--- default.nix
+++ default.nix
@@ -6,6 +6,7 @@ in {
   reflex-dom-core = haskellPackages.callPackage
     ({ temporary, jsaddle-warp, process, chrome-test-utils
      , selenium-server-standalone, which, fontconfig, chromium, pkgs
+     , ...
      }: let
       inherit (haskellPackages) ghc;
       noGcTest = stdenv.hostPlatform.system != "x86_64-linux"
