2dd03d766edf7896eab85f68b2ba718f7884ff6c
Elliot Cameron <eacameron@gmail.com>
2020-07-22 17:06:28 -0400

    Use consistent indentation in MainWidget.java

--- reflex-dom/java/org/reflexfrp/reflexdom/MainWidget.java
+++ reflex-dom/java/org/reflexfrp/reflexdom/MainWidget.java
@@ -50,84 +50,84 @@ public class MainWidget {
     final AtomicBoolean jsaddleLoaded = new AtomicBoolean(false);
 
     wv.setWebViewClient(new WebViewClient() {
-        @Override
-        public void onPageFinished(WebView _view, String _url) {
-          Log.i("reflex", "onPageFinished");
-          boolean alreadyLoaded = jsaddleLoaded.getAndSet(true);
-          if(!alreadyLoaded) {
-            Log.i("reflex", "loading jsaddle");
-            wv.evaluateJavascript(initialJS, null);
+      @Override
+      public void onPageFinished(WebView _view, String _url) {
+        Log.i("reflex", "onPageFinished");
+        boolean alreadyLoaded = jsaddleLoaded.getAndSet(true);
+        if(!alreadyLoaded) {
+          Log.i("reflex", "loading jsaddle");
+          wv.evaluateJavascript(initialJS, null);
+        }
+      }
+
+      // Re-route / to /android_asset
+      @Override
+      public WebResourceResponse shouldInterceptRequest (WebView view, WebResourceRequest request) {
+        Uri uri = request.getUrl();
+        if(!uri.getScheme().equals("file"))
+          return null;
+
+        String path = uri.getPath();
+        path = getAssetPath(path);
+
+        String mimeType = getMimeType(uri.toString());
+        String encoding = "";
+
+        try {
+          InputStream data = a.getApplicationContext().getAssets().open(path);
+          return new WebResourceResponse(mimeType, encoding, data);
+        }
+        catch (IOException e) {
+          Log.i("reflex", "Opening resource failed, Webview will handle the request ..");
+          e.printStackTrace();
+        }
+
+        return null;
+      }
+
+      @Override
+      public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        if(url != null && !url.startsWith("http://") && !url.startsWith("https://") && !url.startsWith("file://")) {
+          try {
+            view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
           }
+          catch(ActivityNotFoundException e) {
+            Log.e("reflex", "Starting activity for intent '" + url + "' failed!");
+          }
+          return true;
+        } else {
+          return false;
         }
-
-        // Re-route / to /android_asset
-        @Override
-        public WebResourceResponse shouldInterceptRequest (WebView view, WebResourceRequest request) {
-            Uri uri = request.getUrl();
-            if(!uri.getScheme().equals("file"))
-                return null;
-
-            String path = uri.getPath();
-            path = getAssetPath(path);
-
-            String mimeType = getMimeType(uri.toString());
-            String encoding = "";
-
-            try {
-                InputStream data = a.getApplicationContext().getAssets().open(path);
-                return new WebResourceResponse(mimeType, encoding, data);
-            }
-            catch (IOException e) {
-                Log.i("reflex", "Opening resource failed, Webview will handle the request ..");
-                e.printStackTrace();
-            }
-
-            return null;
-        }
-
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if( url != null && !url.startsWith("http://") && !url.startsWith("https://") && !url.startsWith("file://")) {
-                try {
-                    view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
-                }
-                catch(ActivityNotFoundException  e) {
-                    Log.e("reflex", "Starting activity for intent '" + url + "' failed!");
-                }
-                return true;
-            } else {
-                return false;
-            }
-        }
+      }
     });
 
     wv.setWebChromeClient(new WebChromeClient() {
-        // Need to accept permissions to use the camera and audio
-        @Override
-        public void onPermissionRequest(final PermissionRequest request) {
-            if(request.getOrigin().toString().startsWith("file://")) {
-                a.requestWebViewPermissions(request);
+      // Need to accept permissions to use the camera and audio
+      @Override
+      public void onPermissionRequest(final PermissionRequest request) {
+        if(request.getOrigin().toString().startsWith("file://")) {
+          a.requestWebViewPermissions(request);
+        }
+        else {
+          a.runOnUiThread(new Runnable() {
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void run() {
+              request.deny();
             }
-            else {
-                a.runOnUiThread(new Runnable() {
-                        @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-                        @Override
-                        public void run() {
-                            request.deny();
-                        }
-                    });
-            }
+          });
         }
+      }
 
-        @Override
-        public Bitmap getDefaultVideoPoster() {
-            return Bitmap.createBitmap(10, 10, Bitmap.Config.ARGB_8888);
-        }
+      @Override
+      public Bitmap getDefaultVideoPoster() {
+        return Bitmap.createBitmap(10, 10, Bitmap.Config.ARGB_8888);
+      }
 
-        @Override
-        public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
-            callback.invoke(origin, true, false);
-        }
+      @Override
+      public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+        callback.invoke(origin, true, false);
+      }
     });
 
     wv.addJavascriptInterface(new JSaddleCallbacks(jsaddleCallbacks), "jsaddle");
@@ -139,29 +139,29 @@ public class MainWidget {
       public final void evaluateJavascript(final byte[] js) {
         final String jsStr = new String(js, StandardCharsets.UTF_8);
         hnd.post(new Runnable() {
-            @Override
-            public void run() {
-              wv.evaluateJavascript(jsStr, null);
-            }
-          });
+          @Override
+          public void run() {
+            wv.evaluateJavascript(jsStr, null);
+          }
+        });
       }
     };
   }
 
   private static String getMimeType(String url) {
-      String type = "";
-      String extension = MimeTypeMap.getFileExtensionFromUrl(url);
-      if (extension != null) {
-          type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
-      }
-      return type;
+    String type = "";
+    String extension = MimeTypeMap.getFileExtensionFromUrl(url);
+    if (extension != null) {
+      type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+    }
+    return type;
   }
 
   /** Get the path of an asset. Strips leading / and leading /android_asset/ */
   private static String getAssetPath(String path) {
-      path = path.startsWith("/android_asset") ? path.substring("/android_asset".length()) : path;
-      path = path.startsWith("/") ? path.substring(1) : path;
-      return path;
+    path = path.startsWith("/android_asset") ? path.substring("/android_asset".length()) : path;
+    path = path.startsWith("/") ? path.substring(1) : path;
+    return path;
   }
 
   private static class JSaddleCallbacks {
