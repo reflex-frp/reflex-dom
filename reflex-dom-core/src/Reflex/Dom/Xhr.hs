d9c933d6a093381ec7e9b43dc590fe6a43929b1b
cidkidnix <cidkidnix@protonmail.com>
2023-04-06 10:29:22 -0500

    Import from Monad instaed of Types

--- reflex-dom-core/src/Reflex/Dom/Xhr.hs
+++ reflex-dom-core/src/Reflex/Dom/Xhr.hs
@@ -21,131 +21,119 @@
 --
 --  * a flexible set of functions for creating and executing arbitrary
 --    requests and handling responses.
---
 module Reflex.Dom.Xhr
   ( -- * Common Patterns
 
     -- | Functions that conveniently expose common uses like GET and
     -- POST to JSON APIs.
-    getAndDecode
-  , getMay
-  , postJson
+    getAndDecode,
+    getMay,
+    postJson,
+    decodeXhrResponse,
+    decodeText,
 
-  , decodeXhrResponse
-  , decodeText
+    -- * General Request API
 
-  -- * General Request API
+    -- | This is the most general flow for sending XHR requests:
+    --
+    --   1. Create an 'Event' stream of 'XhrRequest' records (ie
+    --   @Event t (XhrRequest a)@). The records configure the request,
+    --   and the 'Event' controls when the request or requests are
+    --   actually sent.
+    --
+    --   2. Plug the @Event t (XhrRequest a)@ into one of the functions
+    --   for performing requests like 'performRequestAsync'.
+    --
+    --   3. Consume the resulting stream of 'XhrResponse' events,
+    --   parsing the body of the response however appropriate. A really
+    --   common pattern is turning the 'Event' into a 'Dynamic' with
+    --   'holdDyn' or a related function.
+    --
+    -- Here is an example of calling a search API whenever the user
+    -- types in a text input field and printing the result on the page:
+    --
+    -- @
+    -- url query = "http:\/\/example.com\/search?query=" \<> query
+    --
+    -- search queries = do
+    --   responses \<- performRequestAsync $ toRequest \<$> queries
+    --   return $ view xhrResponse_responseText \<$> responses
+    --   where toRequest query = XhrRequest \"GET" (url query) def
+    --
+    -- main = mainWidget $ do
+    --   input \<- textInput def
+    --   let queries = updated $ input ^. textInput_value
+    --   results \<- search queries
+    --   asText \<- holdDyn "No results." $ pack . show \<$> results
+    --   dynText asText
+    -- @
 
-  -- | This is the most general flow for sending XHR requests:
-  --
-  --   1. Create an 'Event' stream of 'XhrRequest' records (ie
-  --   @Event t (XhrRequest a)@). The records configure the request,
-  --   and the 'Event' controls when the request or requests are
-  --   actually sent.
-  --
-  --   2. Plug the @Event t (XhrRequest a)@ into one of the functions
-  --   for performing requests like 'performRequestAsync'.
-  --
-  --   3. Consume the resulting stream of 'XhrResponse' events,
-  --   parsing the body of the response however appropriate. A really
-  --   common pattern is turning the 'Event' into a 'Dynamic' with
-  --   'holdDyn' or a related function.
-  --
-  -- Here is an example of calling a search API whenever the user
-  -- types in a text input field and printing the result on the page:
-  --
-  -- @
-  -- url query = "http:\/\/example.com\/search?query=" \<> query
-  --
-  -- search queries = do
-  --   responses \<- performRequestAsync $ toRequest \<$> queries
-  --   return $ view xhrResponse_responseText \<$> responses
-  --   where toRequest query = XhrRequest \"GET" (url query) def
-  --
-  -- main = mainWidget $ do
-  --   input \<- textInput def
-  --   let queries = updated $ input ^. textInput_value
-  --   results \<- search queries
-  --   asText \<- holdDyn "No results." $ pack . show \<$> results
-  --   dynText asText
-  -- @
+    -- ** XHR Requests
+    XhrRequest (..),
+    XhrRequestConfig (..),
+    xhrRequest,
+    xhrRequestConfig_headers,
+    xhrRequestConfig_password,
+    xhrRequestConfig_responseType,
+    xhrRequestConfig_sendData,
+    xhrRequestConfig_user,
+    xhrRequestConfig_withCredentials,
+    xhrRequestConfig_responseHeaders,
+    xhrRequest_config,
+    xhrRequest_method,
+    xhrRequest_url,
 
-  -- ** XHR Requests
-  , XhrRequest (..)
-  , XhrRequestConfig (..)
+    -- ** Performing Requests
+    performMkRequestAsync,
+    performMkRequestsAsync,
+    performRequestAsync,
+    performRequestAsyncWithError,
+    performRequestsAsync,
+    performRequestsAsyncWithError,
 
-  , xhrRequest
-  , xhrRequestConfig_headers
-  , xhrRequestConfig_password
-  , xhrRequestConfig_responseType
-  , xhrRequestConfig_sendData
-  , xhrRequestConfig_user
-  , xhrRequestConfig_withCredentials
-  , xhrRequestConfig_responseHeaders
-  , xhrRequest_config
-  , xhrRequest_method
-  , xhrRequest_url
+    -- ** XHR Responses
+    XhrResponse (..),
+    XhrResponseBody (..),
+    XhrResponseHeaders (..),
+    XhrResponseType (..),
+    xhrResponse_response,
+    xhrResponse_responseText,
+    xhrResponse_status,
+    xhrResponse_statusText,
+    xhrResponse_headers,
 
-  -- ** Performing Requests
-  , performMkRequestAsync
-  , performMkRequestsAsync
-  , performRequestAsync
-  , performRequestAsyncWithError
-  , performRequestsAsync
-  , performRequestsAsyncWithError
+    -- *** Deprecated
+    xhrResponse_body,
+    _xhrResponse_body,
 
-  -- ** XHR Responses
-  , XhrResponse (..)
-  , XhrResponseBody (..)
-  , XhrResponseHeaders (..)
-  , XhrResponseType (..)
+    -- ** Error Handling
+    XhrException (..),
+    IsXhrPayload (..),
 
-  , xhrResponse_response
-  , xhrResponse_responseText
-  , xhrResponse_status
-  , xhrResponse_statusText
-  , xhrResponse_headers
+    -- * JavaScript XMLHttpRequest Objects
 
-  -- *** Deprecated
-  , xhrResponse_body
-  , _xhrResponse_body
+    -- | 'XMLHttpRequest' is the type of JavaScript's underlying runtime
+    -- objects that represent XHR requests.
+    --
+    -- Chances are you shouldn't need these in day-to-day code.
+    XMLHttpRequest,
 
-  -- ** Error Handling
-  , XhrException (..)
-  , IsXhrPayload (..)
+    -- ** Constructors
+    newXMLHttpRequest,
+    newXMLHttpRequestWithError,
 
-  -- * JavaScript XMLHttpRequest Objects
-
-  -- | 'XMLHttpRequest' is the type of JavaScript's underlying runtime
-  -- objects that represent XHR requests.
-  --
-  -- Chances are you shouldn't need these in day-to-day code.
-  , XMLHttpRequest
-
-  -- ** Constructors
-  , newXMLHttpRequest
-  , newXMLHttpRequestWithError
-
-  -- ** Fields
-  , xmlHttpRequestGetReadyState
-  , xmlHttpRequestGetResponseText
-  , xmlHttpRequestGetStatus
-  , xmlHttpRequestGetStatusText
-  , xmlHttpRequestNew
-  , xmlHttpRequestOnreadystatechange
-  , xmlHttpRequestOpen
-  , xmlHttpRequestSetRequestHeader
-  , xmlHttpRequestSetResponseType
+    -- ** Fields
+    xmlHttpRequestGetReadyState,
+    xmlHttpRequestGetResponseText,
+    xmlHttpRequestGetStatus,
+    xmlHttpRequestGetStatusText,
+    xmlHttpRequestNew,
+    xmlHttpRequestOnreadystatechange,
+    xmlHttpRequestOpen,
+    xmlHttpRequestSetRequestHeader,
+    xmlHttpRequestSetResponseType,
   )
 where
-
-import Reflex.Class
-import Reflex.Dom.Class
-import Reflex.PerformEvent.Class
-import Reflex.TriggerEvent.Class
-import Reflex.Dom.Xhr.Exception
-import Reflex.Dom.Xhr.Foreign
-import Reflex.Dom.Xhr.ResponseType
 
 import Control.Concurrent
 import Control.Exception (handle)
@@ -153,6 +141,13 @@ import Control.Lens
 import Control.Monad hiding (forM)
 import Control.Monad.IO.Class
 import Data.Aeson
+import Reflex.Class
+import Reflex.Dom.Class
+import Reflex.Dom.Xhr.Exception
+import Reflex.Dom.Xhr.Foreign
+import Reflex.Dom.Xhr.ResponseType
+import Reflex.PerformEvent.Class
+import Reflex.TriggerEvent.Class
 #if MIN_VERSION_aeson(1,0,0)
 import Data.Aeson.Text
 #else
@@ -174,39 +169,40 @@ import qualified Data.Text.Lazy as LT
 import qualified Data.Text.Lazy.Builder as B
 import Data.Traversable
 import Data.Typeable
+import Language.Javascript.JSaddle.Monad (JSM, MonadJSM, askJSM, liftJSM, runJSM)
 
-import Language.Javascript.JSaddle.Types (JSM, askJSM, runJSM, MonadJSM, liftJSM)
+data XhrRequest a = XhrRequest
+  { _xhrRequest_method :: Text,
+    _xhrRequest_url :: Text,
+    _xhrRequest_config :: XhrRequestConfig a
+  }
+  deriving (Show, Read, Eq, Ord, Typeable, Functor)
 
-data XhrRequest a
-   = XhrRequest { _xhrRequest_method :: Text
-                , _xhrRequest_url :: Text
-                , _xhrRequest_config :: XhrRequestConfig a
-                }
-   deriving (Show, Read, Eq, Ord, Typeable, Functor)
+data XhrRequestConfig a = XhrRequestConfig
+  { _xhrRequestConfig_headers :: Map Text Text,
+    _xhrRequestConfig_user :: Maybe Text,
+    _xhrRequestConfig_password :: Maybe Text,
+    _xhrRequestConfig_responseType :: Maybe XhrResponseType,
+    _xhrRequestConfig_sendData :: a,
+    _xhrRequestConfig_withCredentials :: Bool,
+    _xhrRequestConfig_responseHeaders :: XhrResponseHeaders
+  }
+  deriving (Show, Read, Eq, Ord, Typeable, Functor)
 
-data XhrRequestConfig a
-   = XhrRequestConfig { _xhrRequestConfig_headers :: Map Text Text
-                      , _xhrRequestConfig_user :: Maybe Text
-                      , _xhrRequestConfig_password :: Maybe Text
-                      , _xhrRequestConfig_responseType :: Maybe XhrResponseType
-                      , _xhrRequestConfig_sendData :: a
-                      , _xhrRequestConfig_withCredentials :: Bool
-                      , _xhrRequestConfig_responseHeaders :: XhrResponseHeaders
-                      }
-   deriving (Show, Read, Eq, Ord, Typeable, Functor)
+data XhrResponse = XhrResponse
+  { _xhrResponse_status :: Word,
+    _xhrResponse_statusText :: Text,
+    _xhrResponse_response :: Maybe XhrResponseBody,
+    _xhrResponse_responseText :: Maybe Text,
+    _xhrResponse_headers :: Map (CI Text) Text
+  }
+  deriving (Typeable)
 
-data XhrResponse
-   = XhrResponse { _xhrResponse_status :: Word
-                 , _xhrResponse_statusText :: Text
-                 , _xhrResponse_response :: Maybe XhrResponseBody
-                 , _xhrResponse_responseText :: Maybe Text
-                 , _xhrResponse_headers :: Map (CI Text) Text
-                 }
-   deriving (Typeable)
-
-data XhrResponseHeaders =
-    OnlyHeaders (Set.Set (CI Text)) -- ^ Parse a subset of headers from the XHR Response
-  | AllHeaders -- ^ Parse all headers from the XHR Response
+data XhrResponseHeaders
+  = -- | Parse a subset of headers from the XHR Response
+    OnlyHeaders (Set.Set (CI Text))
+  | -- | Parse all headers from the XHR Response
+    AllHeaders
   deriving (Show, Read, Eq, Ord, Typeable)
 
 instance Default XhrResponseHeaders where
@@ -218,17 +214,19 @@ _xhrResponse_body = _xhrResponse_responseText
 
 {-# DEPRECATED xhrResponse_body "Use xhrResponse_response or xhrResponse_responseText instead." #-}
 xhrResponse_body :: Lens' XhrResponse (Maybe Text)
-xhrResponse_body = lens _xhrResponse_responseText (\r t -> r { _xhrResponse_responseText = t })
+xhrResponse_body = lens _xhrResponse_responseText (\r t -> r {_xhrResponse_responseText = t})
 
 instance a ~ () => Default (XhrRequestConfig a) where
-  def = XhrRequestConfig { _xhrRequestConfig_headers = Map.empty
-                         , _xhrRequestConfig_user = Nothing
-                         , _xhrRequestConfig_password  = Nothing
-                         , _xhrRequestConfig_responseType  = Nothing
-                         , _xhrRequestConfig_sendData  = ()
-                         , _xhrRequestConfig_withCredentials = False
-                         , _xhrRequestConfig_responseHeaders = def
-                         }
+  def =
+    XhrRequestConfig
+      { _xhrRequestConfig_headers = Map.empty,
+        _xhrRequestConfig_user = Nothing,
+        _xhrRequestConfig_password = Nothing,
+        _xhrRequestConfig_responseType = Nothing,
+        _xhrRequestConfig_sendData = (),
+        _xhrRequestConfig_withCredentials = False,
+        _xhrRequestConfig_responseHeaders = def
+      }
 
 -- | Construct a request object from method, URL, and config record.
 xhrRequest :: Text -> Text -> XhrRequestConfig a -> XhrRequest a
@@ -238,15 +236,15 @@ xhrRequest = XhrRequest
 -- and returns an XHR object immediately (which you can use to abort
 -- the XHR connection), and will pass an exception ('XhrException') to the
 -- continuation if the connection cannot be made (or is aborted).
-newXMLHttpRequestWithError
-    :: (MonadJSM m, IsXhrPayload a)
-    => XhrRequest a
-    -- ^ The request to make.
-    -> (Either XhrException XhrResponse -> JSM ())
-    -- ^ A continuation to be called once a response comes back, or in
-    -- case of error.
-    -> m XMLHttpRequest
-    -- ^ The XHR request, which could for example be aborted.
+newXMLHttpRequestWithError ::
+  (MonadJSM m, IsXhrPayload a) =>
+  -- | The request to make.
+  XhrRequest a ->
+  -- | A continuation to be called once a response comes back, or in
+  -- case of error.
+  (Either XhrException XhrResponse -> JSM ()) ->
+  -- | The XHR request, which could for example be aborted.
+  m XMLHttpRequest
 newXMLHttpRequestWithError req cb = do
   xhr <- xmlHttpRequestNew
   ctx <- askJSM
@@ -269,31 +267,43 @@ newXMLHttpRequestWithError req cb = do
       status <- xmlHttpRequestGetStatus xhr
       statusText <- xmlHttpRequestGetStatusText xhr
       when (readyState == 4) $ do
-        t <- if rt == Just XhrResponseType_Text || isNothing rt
-             then xmlHttpRequestGetResponseText xhr
-             else return Nothing
+        t <-
+          if rt == Just XhrResponseType_Text || isNothing rt
+            then xmlHttpRequestGetResponseText xhr
+            else return Nothing
         r <- xmlHttpRequestGetResponse xhr
         h <- case _xhrRequestConfig_responseHeaders c of
-          AllHeaders -> parseAllHeadersString <$>
-            xmlHttpRequestGetAllResponseHeaders xhr
-          OnlyHeaders xs -> traverse (xmlHttpRequestGetResponseHeader xhr)
-            (Map.fromSet CI.original xs)
-        _ <- liftJSM $ cb $ Right
-             XhrResponse { _xhrResponse_status = status
-                         , _xhrResponse_statusText = statusText
-                         , _xhrResponse_response = r
-                         , _xhrResponse_responseText = t
-                         , _xhrResponse_headers = h
-                         }
+          AllHeaders ->
+            parseAllHeadersString
+              <$> xmlHttpRequestGetAllResponseHeaders xhr
+          OnlyHeaders xs ->
+            traverse
+              (xmlHttpRequestGetResponseHeader xhr)
+              (Map.fromSet CI.original xs)
+        _ <-
+          liftJSM $
+            cb $
+              Right
+                XhrResponse
+                  { _xhrResponse_status = status,
+                    _xhrResponse_statusText = statusText,
+                    _xhrResponse_response = r,
+                    _xhrResponse_responseText = t,
+                    _xhrResponse_headers = h
+                  }
         return ()
     _ <- xmlHttpRequestSend xhr (_xhrRequestConfig_sendData c)
     return ()
   return xhr
 
 parseAllHeadersString :: Text -> Map (CI Text) Text
-parseAllHeadersString s = Map.fromList $ fmap (stripBoth . T.span (/=':')) $
-  L.dropWhileEnd T.null $ T.splitOn (T.pack "\r\n") s
-  where stripBoth (txt1, txt2) = (CI.mk $ T.strip txt1, T.strip $ T.drop 1 txt2)
+parseAllHeadersString s =
+  Map.fromList $
+    fmap (stripBoth . T.span (/= ':')) $
+      L.dropWhileEnd T.null $
+        T.splitOn (T.pack "\r\n") s
+  where
+    stripBoth (txt1, txt2) = (CI.mk $ T.strip txt1, T.strip $ T.drop 1 txt2)
 
 newXMLHttpRequest :: (MonadJSM m, IsXhrPayload a) => XhrRequest a -> (XhrResponse -> JSM ()) -> m XMLHttpRequest
 newXMLHttpRequest req cb = newXMLHttpRequestWithError req $ mapM_ cb
@@ -303,10 +313,10 @@ newXMLHttpRequest req cb = newXMLHttpRequestWithError req $ mapM_ cb
 --
 -- The request is processed asynchronously, therefore handling does
 -- not block or cause a delay while creating the connection.
-performRequestAsyncWithError
-    :: (MonadJSM (Performable m), PerformEvent t m, TriggerEvent t m, IsXhrPayload a)
-    => Event t (XhrRequest a)
-    -> m (Event t (Either XhrException XhrResponse))
+performRequestAsyncWithError ::
+  (MonadJSM (Performable m), PerformEvent t m, TriggerEvent t m, IsXhrPayload a) =>
+  Event t (XhrRequest a) ->
+  m (Event t (Either XhrException XhrResponse))
 performRequestAsyncWithError = performRequestAsync' newXMLHttpRequestWithError . fmap return
 
 -- | Given Event of request, issue them when the Event fires.  Returns Event of corresponding response.
@@ -332,9 +342,10 @@ performRequestAsync' newXhr req = performEventAsync $ ffor req $ \hr cb -> do
 --
 -- Order of request execution and completion is not guaranteed, but
 -- order of creation and the collection result is preserved.
-performRequestsAsyncWithError
-    :: (MonadJSM (Performable m), PerformEvent t m, TriggerEvent t m, Traversable f, IsXhrPayload a)
-    => Event t (f (XhrRequest a)) -> m (Event t (f (Either XhrException XhrResponse)))
+performRequestsAsyncWithError ::
+  (MonadJSM (Performable m), PerformEvent t m, TriggerEvent t m, Traversable f, IsXhrPayload a) =>
+  Event t (f (XhrRequest a)) ->
+  m (Event t (f (Either XhrException XhrResponse)))
 performRequestsAsyncWithError = performRequestsAsync' newXMLHttpRequestWithError . fmap return
 
 -- | Issues a collection of requests when the supplied Event fires.  When ALL requests from a given firing complete, the results are collected and returned via the return Event.
@@ -364,16 +375,19 @@ getAndDecode url = do
 -- | Create a "POST" request from an URL and thing with a JSON representation
 postJson :: (ToJSON a) => Text -> a -> XhrRequest Text
 postJson url a =
-  XhrRequest "POST" url $ def { _xhrRequestConfig_headers = headerUrlEnc
-                              , _xhrRequestConfig_sendData = body
-                              }
-  where headerUrlEnc = "Content-type" =: "application/json"
-        body = LT.toStrict $ B.toLazyText $ encodeToTextBuilder $ toJSON a
+  XhrRequest "POST" url $
+    def
+      { _xhrRequestConfig_headers = headerUrlEnc,
+        _xhrRequestConfig_sendData = body
+      }
+  where
+    headerUrlEnc = "Content-type" =: "application/json"
+    body = LT.toStrict $ B.toLazyText $ encodeToTextBuilder $ toJSON a
 
 getMay :: (Monad m, Reflex t) => (Event t a -> m (Event t b)) -> Event t (Maybe a) -> m (Event t (Maybe b))
 getMay f e = do
-    e' <- f (fmapMaybe id e)
-    return $ leftmost [fmap Just e', fmapMaybe (maybe (Just Nothing) (const Nothing)) e]
+  e' <- f (fmapMaybe id e)
+  return $ leftmost [fmap Just e', fmapMaybe (maybe (Just Nothing) (const Nothing)) e]
 
 decodeText :: FromJSON a => Text -> Maybe a
 decodeText = decode . BL.fromStrict . encodeUtf8
